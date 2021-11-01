33005c15f5587f6ce4f04d497e76065516b1e19e
Siyuexi <798034039@qq.com>
2021-11-01 22:50:06 +0800

    --Updated=更新epoch与训练结果

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ log = open('log/resnet50-finetuning-log.txt','wt')
 """
 
 # 超参数设置
-num_epochs = 60   
+num_epochs = 100   
 num_classes = 5
 print_period = 5 # print per [print_period] batch
 lossmix_period = 0 # mix loss from [lossmix_period] epoch
